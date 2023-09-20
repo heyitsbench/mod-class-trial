0983c1cd61cc0b3d5deeb1129dec3733b7c5a4e7
benjymansy123 <benjymansy123@gmail.com>
2023-09-20 00:48:45 -0400

    Init

---
+++ src/class_trial.cpp
@@ -0,0 +1,9 @@
+#include "class_trial_loader.h"
+
+void Add_class_trial()
+{
+}
+
+void AddSC_class_trial_commandscript()
+{
+}
---
+++ src/class_trial_loader.h
@@ -0,0 +1,8 @@
+void Add_class_trial();
+void AddSC_class_trial_commandscript();
+
+void Addmod_class_trialScripts()
+{
+    Add_class_trial();
+    AddSC_class_trial_commandscript();
+}
